d37d8609d8e1b13937cfb6be4ad75f0768e08718
Tingting <tingting8523@gmail.com>
2020-02-10 21:30:59 +0100

    changes in testfile

--- src/test/java/DummyTest.java
+++ src/test/java/DummyTest.java
@@ -1,6 +1,10 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+
 public class DummyTest {
+    @Test
     void testDummy(){
         assertEquals(true,Dummy.dummyMethod());
     }
